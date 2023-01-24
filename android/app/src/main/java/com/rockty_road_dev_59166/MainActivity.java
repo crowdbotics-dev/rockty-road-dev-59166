9277ab8c2805d323510a0789e023c3a7f3c053b8
Crowdbotics <engineering@crowdbotics.com>
2023-01-24 14:22:23 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/rockty_road_dev_59166/MainActivity.java
@@ -0,0 +1,15 @@
+package com.rockty_road_dev_59166;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "rockty_road_dev_59166";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "rockty-road-dev-59166",
+  url: "https://rockty-road-dev-59166.botics.co",
+  api: "https://rockty-road-dev-59166.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
